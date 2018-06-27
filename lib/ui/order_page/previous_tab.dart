6dfab61983d6e3b4969bb7ec5782b470b85bfb64
Andrew Nero <andrew.nero@gmail.com>
2018-06-27 00:00:02 -0400

    extra

--- lib/ui/order_page/previous_tab.dart
+++ lib/ui/order_page/previous_tab.dart
@@ -12,26 +12,26 @@ class PreviousTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return new StreamBuilder(
         stream: refSelections.where('status', isEqualTo: 'complete').where('uid', isEqualTo: globals.currentUser.id)
-            .orderBy('date').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return new Text('Loading');
-          else if (snapshot.data.documents.length == 0)
-            return new Center(
-                child: Image.asset(
-                  'assets/images/empty-previous.png',
-                  width: 255.0,
-                  fit: BoxFit.fitHeight,
-                ));
-          return new ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              padding: const EdgeInsets.only(bottom: 2.0, top: 8.0),
-              itemBuilder: (context, index) => new SelectionListItem(
-                context: context,
-                selection: Selection
-                    .fromSelectionDoc(snapshot.data.documents[index]),
-                fromShoppingPage: false,
-              ));
-        });
-  }
+            .limit(5).orderBy('date').snapshots(),
+builder: (context, snapshot) {
+if (!snapshot.hasData)
+return new Text('Loading');
+else if (snapshot.data.documents.length == 0)
+return new Center(
+child: Image.asset(
+'assets/images/empty-previous.png',
+width: 255.0,
+fit: BoxFit.fitHeight,
+));
+return new ListView.builder(
+itemCount: snapshot.data.documents.length,
+padding: const EdgeInsets.only(bottom: 2.0, top: 8.0),
+itemBuilder: (context, index) => new SelectionListItem(
+context: context,
+selection: Selection
+    .fromSelectionDoc(snapshot.data.documents[index]),
+fromShoppingPage: false,
+));
+});
+}
 }
